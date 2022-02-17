cc5d05152b0148bd2e257d74e32099b261afffaf
Gilberto Cezar de Almeida <gibalmeida@gmail.com>
2022-02-17 09:18:33 -0300

    Removes todo anotations

--- lib/presentation/pages/auth_gate/auth_gate.dart
+++ lib/presentation/pages/auth_gate/auth_gate.dart
@@ -2,6 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
 
+//TODO: incluir logotipo da empresa
+//TODO: traduzir textos
+
 class AuthGate extends StatelessWidget {
   const AuthGate({Key? key, required this.child}) : super(key: key);
   final Widget child;
